c34cc6e16755831a728153f46e69d6e73c072e9d
john Doe <john@gmail.com>
2024-04-17 12:32:39 +0530

    added project and resume pages

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,10 +3,27 @@ import "./header.scss";
 import meImg from "../../assets/img/me.png";
 import { animate, motion } from "framer-motion";
 
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
 const Header = () => {
   return (
     <div className="container">
-      <div className="header-box">
+      <motion.div className="header-box">
         <div className="img-container">
           <img src={meImg} alt="" />
         </div>
@@ -15,7 +32,7 @@ const Header = () => {
             <h3>Hello</h3>
           </div>
           <div className="para-text">
-            <p>
+            <motion.p>
               Hi, I'm Mirza Mukhtadar, a frontend developer passionate about
               transforming ideas into captivating digital experiences. With
               expertise in HTML, CSS, JavaScript, and frontend frameworks, I
@@ -23,7 +40,7 @@ const Header = () => {
               users. From concept to execution, I thrive on delivering
               innovative solutions that elevate brands and engage audiences.
               Let's collaborate and bring your vision to life.
-            </p>
+            </motion.p>
           </div>
           <div className="btn-container">
             <motion.button
@@ -45,14 +62,14 @@ const Header = () => {
             <motion.button
               whileHover={{
                 scale: 1.1,
-                transition: { duration: 1 },
+                transition: { duration: 1.5 },
               }}
             >
               Contact
             </motion.button>
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
